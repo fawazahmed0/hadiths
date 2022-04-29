4eb749db8d31cb017cff7bc1c7d7a602803c8ecd
Fawaz Ahmed <fawazahmed0@hotmail.com>
2022-04-29 14:42:32 +0530

    dont' use strechedlinks, rather have links at footer

--- template/commoncode.js
+++ template/commoncode.js
@@ -8,7 +8,6 @@ let htmlHadithContainer =
 <span id="footercontainer">
 </span>
 
-<a href="" class="stretched-link"></a>
 </div>
 `
 
@@ -92,19 +91,18 @@ function getHadithCardElem(hadith,dirval,lang,isocodes){
     cardElem.querySelector('.card-footer').insertAdjacentHTML("beforeend", `<b>${capitalize(grade.grade)}</b> : ${grade.name}<br>`);
     if(hadith.hadithnumber){
       cardElem.querySelector('#footercontainer').appendChild(footerDiv.cloneNode())
-    Array.from(cardElem.querySelectorAll('.card-footer')).at(-1).insertAdjacentHTML("beforeend", `Hadith Number: ${hadith.hadithnumber}<br>`);
+    Array.from(cardElem.querySelectorAll('.card-footer')).at(-1).insertAdjacentHTML("beforeend", `<a href=#hadith${hadith.hadithnumber} class="link-dark text-decoration-none" >Hadith Number: ${hadith.hadithnumber}</a><br>`);
     }
     if(hadith.arabicnumber){
       cardElem.querySelector('#footercontainer').appendChild(footerDiv.cloneNode())
-    Array.from(cardElem.querySelectorAll('.card-footer')).at(-1).insertAdjacentHTML("beforeend", `Arabic Number: ${hadith.arabicnumber}<br>`);
+    Array.from(cardElem.querySelectorAll('.card-footer')).at(-1).insertAdjacentHTML("beforeend", `<a href=#hadith${hadith.hadithnumber} class="link-dark text-decoration-none" >Arabic Number: ${hadith.arabicnumber}</a><br>`);
     }
 
     if(hadith.reference){
       cardElem.querySelector('#footercontainer').appendChild(footerDiv.cloneNode())
-    Array.from(cardElem.querySelectorAll('.card-footer')).at(-1).insertAdjacentHTML("beforeend", `Reference: ${Object.entries(hadith.reference).flat().map(e=>capitalize(e)).join(' ')}<br>`);
+    Array.from(cardElem.querySelectorAll('.card-footer')).at(-1).insertAdjacentHTML("beforeend", `<a href=#hadith${hadith.hadithnumber} class="link-dark text-decoration-none" >Reference: ${Object.entries(hadith.reference).flat().map(e=>capitalize(e)).join(' ')}</a><br>`);
     }
     cardElem.setAttribute('id','hadith'+hadith.hadithnumber)
-    cardElem.querySelector('a').setAttribute('href','#hadith'+hadith.hadithnumber)
 
     cardElem.querySelector('.card-text').setAttribute('dir',dirval)
     cardElem.querySelector('.card-text').setAttribute('lang',isocodes[lowerLang].iso1 ? isocodes[lowerLang].iso1 : isocodes[lowerLang].iso2)
